37d7e9833a2e08ea734f74232e5c3872bfb61af4
dmamadou <mamadou.diallo@nevorus.com>
2026-06-22 09:52:14 +0000

    fix error deployment

--- src/main/java/com/itma/gestionProjet/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/itma/gestionProjet/security/UserRegistrationSecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
